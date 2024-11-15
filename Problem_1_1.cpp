d60816c2089396369c7d281771e1741d041652fc
Abdallah Ahmed <ahmed.apdallah1098@icloud.com>
2024-11-15 15:30:54 +0200

    Create Problem_1_1.cpp

---
+++ Problem_1_1.cpp
@@ -0,0 +1,14 @@
+//" Problem_1 >> Write a program to print your name on screen "
+#include <iostream> 
+using namespace std;
+
+void PrintName(string Name) 
+{
+	cout << "Your Name Is : " << Name << endl;
+}
+
+int main()
+{
+	PrintName("Abdallah Ahmed");
+	return 0;
+}
