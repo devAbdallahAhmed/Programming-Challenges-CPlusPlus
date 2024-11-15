45d0f03b7995afc3f1b788cf038fa5f8b57fc834
Abdallah Ahmed <ahmed.apdallah1098@icloud.com>
2024-11-15 23:54:05 +0200

    Create Problem_6_1.cpp

---
+++ Problem_6_1.cpp
@@ -0,0 +1,19 @@
+// Problem_46 >> Print Letter From a to z
+// Write a program to print all letters from A to Z
+ #include <iostream>
+#include <string>
+ using namespace std;
+
+
+ void Print_Char() {
+	 for (int i = 65; i <= 90; i++) {
+		 cout << char(i) << endl;
+	 }
+ }
+
+int main(){ 
+	Print_Char();
+
+
+return 0;
+} 
