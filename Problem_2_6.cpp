41dfd23a98891dbc8b0f4adb0dcf690addd3fe18
Abdallah Ahmed <ahmed.apdallah1098@icloud.com>
2024-11-15 17:03:58 +0200

    Create Problem_2_6.cpp

---
+++ Problem_2_6.cpp
@@ -0,0 +1,35 @@
+//Problem_15 >>Rectangle Area Write a program to calculate rectangle area and print it on the screen 
+// Area = a*b 
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void Read_LengthWidth(float &A , float &B)
+{
+	
+	cout << " The Enter The Length : ";
+	cin >> A;
+
+	cout << " The Enter The Width : ";
+	cin >> B;
+}
+
+float CalcRectangleArea(float A, float B)
+{
+	return A * B;
+}
+
+void PrintResultArea(float RectangleArea)
+{
+	cout << "The Rectangle Area is : " << RectangleArea << endl;
+}
+
+int main()
+{
+	float A, B;
+	Read_LengthWidth(A, B);
+	PrintResultArea(CalcRectangleArea(A, B));
+	return 0;
+}
